03483747d7adc309702530e9d563c202e660e989
Victor Santos <victorsantos.hf@gmail.com>
2020-08-05 22:08:10 -0300

    ajsutando o footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -8,7 +8,13 @@ function Footer() {
         <img src="https://www.alura.com.br/assets/img/alura-logo-white.1570550707.svg" alt="Logo Alura" />
       </a>
       <p>
-        Orgulhosamente criado durante a
+        Criado Orgulhosamente por
+        {' '}
+        <a href="www.linkedin.com/in/victor-santos-75faluu41">
+          Victor Santos
+         </a>
+        {' '}
+         durante a
         {' '}
         <a href="https://www.alura.com.br/">
           Imersão React da Alura
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -12,7 +12,6 @@ function Home() {
     // http://localhost:8080/categorias?_embed=videos
     categoriasRepository.getAllWithVideos()
       .then((categoriasComVideos) => {
-        console.log(categoriasComVideos[0].videos[0]);
         setDadosIniciais(categoriasComVideos);
       })
       .catch((err) => {
